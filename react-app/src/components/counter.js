6b2cf531752f81890ce0f18f0c457c9ed4b2cc9f
KENNETH EZEKIEL <16521040@mahasiswa.itb.ac.id>
2022-08-12 01:27:00 +0700

    reinstalled react-app

---
+++ react-app/src/components/counter.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = { count : 0 } 
+    render() { 
+        return (
+        <div><h1>Hello</h1><button>+</button></div>
+        );
+    }
+}
+ 
+export default Counter;
